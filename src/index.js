6d8ecccc633c450d4771a02f698ec8839e7787ed
Mike Wolf <mw@mike-wolf.com>
2019-10-27 17:08:32 -0400

    export as component

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Demo from "./demo";
 export default Demo;
+export {Demo}
 const selector = document.querySelector("#componentroot");
 if (selector) ReactDOM.render(<Demo />, selector);
